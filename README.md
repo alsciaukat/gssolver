6b7798fcaa8e295aa1713f2464b1307d1da54de3
alsciaukat <alsciokat@gmail.com>
2025-09-21 11:31:51 +0900

    use gnu++23, add outline

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# gssolver
-Solve for psi in Grad-Shafranov equation using finite difference method.
+# GS Solver
+
+Solve for $\psi(r, z)$ in Grad-Shafranov equation:
+```math
+r\frac{\partial}{\partial r}\left(\frac{1}{r}\frac{\partial \psi}{\partial r}\right) + \frac{\partial^2\psi}{\partial z^2} = -\mu_0 r^2 \frac{dp}{d\psi} - \frac{1}{2} \frac{d F^2}{d\psi}
+```
+using finite difference method.
+
+
+## Build
+
+This project uses CMake 4.
+
+Execute the following to compile the executable `solver`
+in build directory `./build/`.
+```sh
+cmake -S . -B build
+cmake --build build
+```
+
+
+## Debug
+
+For your convenience, `debug` target
+is provided.
+```sh
+cmake --build build --target debug
+```
+This will execute `gdb` on the compiled executable `solver`.
+
+Every executable is compiled with debugging information by default.
+You can feel free to use what you prefer.
--- src/solver.cpp
+++ src/solver.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include "base.hpp"
+#include <cmath>
+#include <numbers>
 
 /*
       Solov'ev's Solution
@@ -17,6 +19,16 @@
 double e_fix = 0.1;
 double e_sor = 0.1;
 double omega = 1.5; // relaxation coefficient
+double A = 1, mu0 = 4*std::numbers::pi*1e-7, sigma = 0.5;
+double Ip = 500e3, psi_bdry = 0.1;
+
+double p_prime(double psi) {
+  return 3*A*A/(4*mu0)*(sigma*sigma - sigma)/std::pow(psi, 7);
+}
+
+double gg_prime(double psi) {
+  return A/(4*std::pow(psi, 3));
+}
 
 bool is_converged(Array<double> psi_p, Array<double> psi) {
   bool converged = true;
@@ -29,14 +41,53 @@ bool is_converged(Array<double> psi_p, Array<double> psi) {
   return converged;
 }
 
+void update_F(const Grid& grid, Array<double>& psi, Array<double>& F) {
+  for (int i = 0; i < grid.N_r; i++) {
+	for (int j = 0; j < grid.N_z; j++) {
+	  F[i][j] = -(mu0*grid.r[i]*grid.r[i]*p_prime(psi[i][j]) + gg_prime(psi[i][j]));
+	}
+  }
+}
+
+void update_sigma(const Grid& grid, const Array<double>& F, double& sigma) {
+  double I = 0;
+
+  for (int i = 0; i < grid.N_r; i++) {
+	for (int j = 0; j < grid.N_z; j++) {
+	  if (grid.boundary[i][j].domain != Domain::INT) continue;
+	  I += F[i][j]/(mu0*grid.r[i])*grid.h*grid.h;
+	}
+  }
+
+  sigma = Ip / (psi_bdry * I);
+}
+
+void normalize(const Grid& grid, Array<double>& psi) {
+  // normalize psi between 0 at the center and 1 at the boundary
+}
+
+void sor(const Grid& grid, Array<double>& psi, Array<double>& F, double& sigma) {
+  // find solution to Grad-Shafranov equaiton using
+  // successive over relation with fixed F and sigma
+}
+
 int main() {
-  // May use std::cin for N
+  // May use std::cin for N in the future
   int N = 10 + 1;
   Grid grid(N);
   Array<double> psi(grid.N_r, std::vector<double>(grid.N_z, 1));
   Array<double> psi_p;
+  Array<double> F(grid.N_r, std::vector<double>(grid.N_z, 0));
+  double sigma;
+
   do {
 	psi_p = psi;
+	update_F(grid, psi, F);
+	update_sigma(grid, F, sigma);
 
+    sor(grid, psi, F, sigma);
+    normalize(grid, psi);
   } while (!is_converged(psi_p, psi));
+
+  std::cout << p_prime(10) << std::endl;
 }
